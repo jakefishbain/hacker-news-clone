b8b1746874fb074a011c0bf8e10d102885709e77
jakefishbain & Jeff Fichtner <pair+jakefishbain.jeff-fichtner@devbootcamp.com>
2016-10-03 18:22:35 -0500

    create user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+  # before_action :check_logged_in, except: [:new]
+  # before_action :find_user, except: [:new]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def comments
+  end
+
+  private
+  def find_user
+    @user ||= User.find_by(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
