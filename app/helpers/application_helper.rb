be67964ab3003f1aaac185c852177d6eb6750341
jakefishbain & Jeff Fichtner <pair+jakefishbain.jeff-fichtner@devbootcamp.com>
2016-10-03 18:22:56 -0500

    helpers and user views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:id])
+  end
+
+  def check_logged_in
+    redirect_to '/users/new' unless logged_in?
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def check_owner(object)
+    current_user == object
+  end
 end
