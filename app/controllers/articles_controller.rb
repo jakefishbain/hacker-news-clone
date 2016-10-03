cbe1abd64fa25931655c7495527b90bc71e582bf
jakefishbain & Jeff Fichtner <pair+jakefishbain.jeff-fichtner@devbootcamp.com>
2016-10-03 16:25:47 -0500

    pass comment to show on article controller for new comment partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,25 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params.merge(author_id: session[:user_id]))
+    if @article.save
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :author_id, :url)
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+
+    @comment = Comment.new(comment: params[:comment], commenter_id: 1, article_id: params[:id])
+    if @comment.save
+      redirect_to article_path
+    else
+      render article_path
+    end
+  end
 end
